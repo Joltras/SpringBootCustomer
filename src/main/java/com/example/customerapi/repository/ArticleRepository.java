a11bf5a2fdcc889e8adc146e6aca812767605025
Joltra <MinnyMouseXDmail.com>
2022-10-06 22:09:43 +0200

    Add documentation for the repository classes

--- src/main/java/com/example/customerapi/repository/ArticleRepository.java
+++ src/main/java/com/example/customerapi/repository/ArticleRepository.java
@@ -3,7 +3,8 @@ package com.example.customerapi.repository;
 import com.example.customerapi.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-}
+/**
+ * JPA repository for article entities.
+ */
+public interface ArticleRepository extends JpaRepository<Article, Long> {}
 
--- src/main/java/com/example/customerapi/repository/CustomerRepository.java
+++ src/main/java/com/example/customerapi/repository/CustomerRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * JPA repository for customer entities.
+ */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-
     List<Customer> findByFirstnameOrLastname(String firstname, String lastname);
 }
