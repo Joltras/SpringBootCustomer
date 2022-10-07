c734b27d520502e3897964cee51807429b7348cd
Joltra <MinnyMouseXDmail.com>
2022-10-07 08:55:54 +0200

    Add documentation to application class

--- src/main/java/com/example/customerapi/CustomerApiApplication.java
+++ src/main/java/com/example/customerapi/CustomerApiApplication.java
@@ -3,11 +3,13 @@ package com.example.customerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class
+ */
 @SpringBootApplication
 public class CustomerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApiApplication.class, args);
 	}
-
 }
