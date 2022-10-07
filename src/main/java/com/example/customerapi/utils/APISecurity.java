9c099f602fc235188ea48fab24b5213921838ccd
Joltra <MinnyMouseXDmail.com>
2022-10-07 08:55:16 +0200

    Remove unused import

--- src/main/java/com/example/customerapi/utils/APISecurity.java
+++ src/main/java/com/example/customerapi/utils/APISecurity.java
@@ -1,13 +1,10 @@
 package com.example.customerapi.utils;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Order(1)
 @EnableWebSecurity
