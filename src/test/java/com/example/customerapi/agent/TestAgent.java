0fd7aaab421cae796d3e1f8fb4a52de109bcb280
Joltra <MinnyMouseXDmail.com>
2022-10-10 18:23:05 +0200

    Remove agent classes

--- src/test/java/com/example/customerapi/agent/TestAgent.java
+++
@@ -1,5 +0,0 @@
-package com.example.customerapi.agent;
-
-@AgentMetaInfo(author = "John Doe", version = "1.0.0", name = "TestAgent")
-public class TestAgent extends AbstractAgent {
-}
--- src/test/java/com/example/customerapi/agent/TestAgent2.java
+++
@@ -1,4 +0,0 @@
-package com.example.customerapi.agent;
-
-public class TestAgent2 extends AbstractAgent {
-}
