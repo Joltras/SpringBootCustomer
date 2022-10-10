4e6fffd84fe38e0bffab555c3971712ea4a3b822
Joltra <MinnyMouseXDmail.com>
2022-10-10 19:58:19 +0200

    Add methods for finding addresses by street and city and add method for counting addresses by city

--- src/main/java/com/example/customerapi/repository/AddressRepository.java
+++ src/main/java/com/example/customerapi/repository/AddressRepository.java
@@ -2,8 +2,17 @@ package com.example.customerapi.repository;
 import com.example.customerapi.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * JPA repository for address entities.
  */
-public interface AddressRepository extends JpaRepository<Address, Long> {}
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findByCity(String city);
+
+    Integer countAddressByCity(String city);
+
+    List<Address> findByStreetOrderByCity(String street);
+
+}
 
